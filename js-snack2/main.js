174d723f378f790434fd67e0a5ba61bc00a9fb4e
MaTtiaa01 <mattia2001@live.it>
2022-10-12 12:09:05 +0200

    add print in page

--- js-snack2/main.js
+++ js-snack2/main.js
@@ -6,12 +6,21 @@ const secondWord = prompt("inserire un altra parola");
 
 //Il software stampa prima la parola più corta, poi la parola più lunga.
 
+let longWordEl = document.querySelector(".long_word");
+let shortWordEl = document.querySelector(".short_word");
+
 if (firstWord.length > secondWord.length) {
     console.log(firstWord);
     console.log(secondWord);
+    longWordEl.innerHTML = firstWord;
+    shortWordEl.innerHTML = secondWord;
 }else if (secondWord.length > firstWord.length) {
     console.log(secondWord);
     console.log(firstWord);
+    shortWordEl.innerHTML = secondWord;
+    longWordEl.innerHTML = firstWord;
 } else{
     console.log("le parole sono lunghe uguali");
+    shortWordEl.innerHTML = "le parole sono lunghe uguali";
+    longWordEl.innerHTML = "le parole sono lunghe uguali";
 }
