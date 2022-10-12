12c4ef56f881f68e20184c8190563065144a09c2
MaTtiaa01 <mattia2001@live.it>
2022-10-12 12:37:22 +0200

    .

---
+++ js-snack3/main.js
@@ -0,0 +1,19 @@
+//Il software deve chiedere per 10 volte all’utente di inserire un numero.
+
+let userNumber;
+
+for (let i = 0; i < 10; i++) {
+    userNumber = Number(prompt("Inserire un numero"));
+    console.log(userNumber);
+    calcolo = userNumber 
+}
+
+
+
+//Il programma stampa la somma di tutti i numeri inseriti. 
+
+let somma = document.querySelector(".somma");
+
+somma.innerHTML = calcolo;
+
+
