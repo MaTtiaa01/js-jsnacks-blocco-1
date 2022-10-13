bf56fe1f3d9a58e3646b18a1c2f82b5ded77b1af
MaTtiaa01 <mattia2001@live.it>
2022-10-13 12:40:37 +0200

    .

---
+++ js-snack4/main.js
@@ -0,0 +1,41 @@
+//In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby,
+
+const nameList = [
+    'mattia',
+    'daniela',
+    'fabio',
+    'riccardo',
+    'andrea'
+]
+
+
+//chiedi all’utente il suo nome 
+
+const userName = prompt("Qual'è il tuo nome?");
+
+//comunicagli se può partecipare o no alla festa.
+let i = 0;
+let nameCheck;
+while (i < nameList.length) {
+    nameCheck = nameList[i]
+    console.log(nameCheck);
+    i++
+}
+
+console.log("sono fuori da while");
+
+if (nameCheck === userName) {
+    console.log("puoi entrate");
+}else{
+    console.log("non puoi entrare");
+}
+
+
+
+// altra soluzione
+
+if (nameList.includes(userName)) {
+    console.log("puoi entrate");
+}else{
+    console.log("non puoi entrare");
+}
